78a61f05c41702197acf1df1d3e37f47f92312d0
latinaround <80864145+latinaround@users.noreply.github.com>
2026-01-14 11:05:44 -0800

    Register EventsGateway provider

--- apps/gateway/src/app.module.ts
+++ apps/gateway/src/app.module.ts
@@ -6,6 +6,7 @@ import { BookingsController } from './controllers/bookings.controller';
 import { ProvidersController } from './controllers/providers.controller';
 import { AlertsController } from './controllers/alerts.controller';
 import { AuthController } from './controllers/auth.controller';
+import { EventsGateway } from './events.gateway';
 
 @Module({
   imports: [
@@ -22,6 +23,6 @@ import { AuthController } from './controllers/auth.controller';
     AlertsController,
     AuthController,
   ],
-  providers: [],
+  providers: [EventsGateway],
 })
 export class AppModule {}
